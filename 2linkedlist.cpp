c128c3e9b5a2121eb3d38fded35634192fc42033
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-05 04:14:59 +0700

    membuat step step

--- 2linkedlist.cpp
+++ 2linkedlist.cpp
@@ -5,7 +5,7 @@ using namespace std;
 struct Node 
 {
     int noMhs;
-    string nama;
+    string name;
     Node* next;
     Node* prev;
 };
@@ -13,5 +13,32 @@ struct Node
 Node* START = NULL;
 
 void addNote() {
+    Node* newNode = new Node(); // step 1: create a new node
+    cout << "\nEnter the roll number of the student: ";
+    cin >> newNode->noMhs;      // Assign Value to the data field of the new node
+    cout << "\nEnter the name of the student: ";
+    cin >> newNode->name;       // Assign Value to the data fieldof the new node
+
+
+    // Insert the new node in the list 
+    if (START == NULL || newNode->noMhs == START->noMhs) {
+        
+        if (START != NULL && newNode->noMhs == START->noMhs) {
+            cout << "\033[31mDuplicate roll numbers not allowed\033]0m" << endl;
+            return;
+
+        }
+         // if the list is empty, make the new node the START
+        newNode->next = START;   // step 3: make the new node point to the first node
+        if (START != NULL) {
+            START->prev = newNode; // step 4 : make the first node point to the new node
+
+    }
+    newNode->prev = NULL;  //step 5 : make the new node point to NULL
+        START = newNode; // step 6 : make the new node the first node
+
+ }
+   else
+  {
     
-}
+  } 
