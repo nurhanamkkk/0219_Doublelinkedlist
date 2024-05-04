9107f0df1708b338934715d0995306505eb01a13
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-05 04:47:05 +0700

    membuat step menu 6

--- 2linkedlist.cpp
+++ 2linkedlist.cpp
@@ -205,6 +205,7 @@ int main ()
           cout << "2. Delete a record from the list" << endl;
           cout << "3. View all records in the ascending order of roll numbers" << endl;
           cout << "4. View all records in the descending order of roll numbers " << endl;
+          cout << "5. Search for a record in the list" << endl;
           
         }
   }
