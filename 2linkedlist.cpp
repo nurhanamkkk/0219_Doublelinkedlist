acfbeb7ad9452873ef4ce4a9ec8d40bd13cf8dc8
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-05 04:50:08 +0700

    membuat case 4

--- 2linkedlist.cpp
+++ 2linkedlist.cpp
@@ -219,6 +219,9 @@ int main ()
                  case '2':
                   deleteNode();
                   break;
+                 case '3':
+                  traverse();
+                  break;
                  
              }
         }
