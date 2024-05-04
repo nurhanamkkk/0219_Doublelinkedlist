19f8b3e3cfd92004746925adb05c65494172dcc5
nurhanamkkk <nurhanamk.30@gmail.com>
2024-05-05 04:20:58 +0700

    membuat step 4

--- 2linkedlist.cpp
+++ 2linkedlist.cpp
@@ -50,8 +50,11 @@ void addNote() {
                                // step 1.c : traverse the list to find the correct position
 
     previous = current;          // step 1.d : move the previous to the current node
+    current = current->next;     // step 1.e : move the current to the next node
 
   }         
+
+  
 
   } 
 
